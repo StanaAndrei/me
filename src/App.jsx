5f20707f998edcf7c25d6bbb8ac9b20d2710aa70
StanaAndrei <stadey33@gmail.com>
2024-06-20 18:36:50 +0300

    set solid router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-
+import { Router, Route } from "@solidjs/router";
+import Home from "./Pages/Home/Home";
+import Contact from "./Pages/Contact/Contact";
 
 function App() {
-  return null
+  return <Router base="/me">
+    <Route path="/" component={Home} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/resume" component={null} />
+  </Router>
 }
 
 export default App
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 /* @refresh reload */
 import { render } from 'solid-js/web'
+import './main.css'
 
 import App from './App'
 
