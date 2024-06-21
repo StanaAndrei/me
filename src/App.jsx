627cab3aa57015429afb57ff1fe5f383b235f6be
StanaAndrei <stadey33@gmail.com>
2024-06-22 01:07:11 +0300

    start sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { Router, Route } from "@solidjs/router";
 import Home from "./Pages/Home/Home";
 import Contact from "./Pages/Contact/Contact";
+import Sidebar from './Components/Sidebar/Sidebar';
+
+
 
 function App() {
-  return <Router base="/me">
-    <Route path="/" component={Home} />
-    <Route path="/contact" component={Contact} />
-    <Route path="/resume" component={null} />
-  </Router>
+  return <div>
+    <Sidebar />
+    <Router base="/me">
+      <Route path="/" component={Home} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/resume" component={null} />
+    </Router>
+  </div>
 }
 
 export default App
