fa29f0f4315fe27191c45e422dd6214d392e6784
StanaAndrei <stadey33@gmail.com>
2024-06-22 15:37:19 +0300

    solved sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { Router, Route } from "@solidjs/router";
 import Home from "./Pages/Home/Home";
 import Contact from "./Pages/Contact/Contact";
 import Sidebar from './Components/Sidebar/Sidebar';
-
-
+import "./App.scss"
 
 function App() {
   return <div>
     <Sidebar />
+    <div className="content">
     <Router base="/me">
+
       <Route path="/" component={Home} />
       <Route path="/contact" component={Contact} />
       <Route path="/resume" component={null} />
     </Router>
+    </div>
   </div>
 }
 
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,13 @@
+import { createEffect, createSignal } from "solid-js";
+import "./Sidebar.scss"
+import fbpic from './../../assets/imgs/fbpic.png'
 
 export default function Sidebar(props) {
   return (
-    <div>
-      
+    <div class="sidebar">
+      <img src={fbpic} alt="" />
+      <a  href="/me/">Home</a>
+      <a  href="/me/contact">Contact</a>
     </div>
   );
 }
